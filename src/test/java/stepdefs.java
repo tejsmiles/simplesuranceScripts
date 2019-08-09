5b94216509d55df6fbe9eecf1e4cdbc392b2e5eb
Tejaswinee <tejaswinee@lapchand.speedport.ip>
2019-08-09 18:43:41 +0200

    Adding suggested missing steps

--- src/test/java/stepdefs.java
+++ src/test/java/stepdefs.java
@@ -1,3 +1,73 @@
+import cucumber.api.java.en.Given;
+
+import cucumber.api.java.en.Then;
+
+import cucumber.api.java.en.When;
+
+
 
 public class stepdefs {
+
+
+
+	@Given("User Login to Dashboard")
+
+	public void user_Login_to_Dashboard() {
+
+		// Write code here that turns the phrase above into concrete actions
+
+		throw new cucumber.api.PendingException();
+
+	}
+
+
+
+	@When("User clicks on ‘create certificate'")
+
+	public void user_clicks_on_create_certificate() {
+
+		// Write code here that turns the phrase above into concrete actions
+
+		throw new cucumber.api.PendingException();
+
+	}
+
+
+
+	@When("User enters the Product and Customer information")
+
+	public void user_enters_the_Product_and_Customer_information() {
+
+		// Write code here that turns the phrase above into concrete actions
+
+		throw new cucumber.api.PendingException();
+
+	}
+
+
+
+	@When("User completes the payment")
+
+	public void user_completes_the_payment() {
+
+		// Write code here that turns the phrase above into concrete actions
+
+		throw new cucumber.api.PendingException();
+
+	}
+
+
+
+	@Then("Certificate is available for download")
+
+	public void certificate_is_available_for_download() {
+
+		// Write code here that turns the phrase above into concrete actions
+
+		throw new cucumber.api.PendingException();
+
+	}
+
+
 }
+
